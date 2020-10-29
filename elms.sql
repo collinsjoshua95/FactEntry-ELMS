5308ce11f159faabf5527f6029a1da5953b959f8
collinsjoshua95 <73641590+collinsjoshua95@users.noreply.github.com>
2020-10-29 15:47:29 +0530

    Add files via upload

---
+++ elms.sql
@@ -0,0 +1,220 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 28, 2020 at 12:15 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `elms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `UserName` varchar(100) NOT NULL,
+  `Password` varchar(100) NOT NULL,
+  `updationDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `UserName`, `Password`, `updationDate`) VALUES
+(1, 'admin', '1beba3bd55e338407fb261aa54a6be71', '2020-10-22 22:18:21'),
+(2, 'factentry', '49cb80f514aaebfee0e25e2e80837303', '2020-10-22 22:25:01');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbldepartments`
+--
+
+CREATE TABLE `tbldepartments` (
+  `id` int(11) NOT NULL,
+  `DepartmentName` varchar(150) DEFAULT NULL,
+  `DepartmentShortName` varchar(100) NOT NULL,
+  `DepartmentCode` varchar(50) DEFAULT NULL,
+  `CreationDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbldepartments`
+--
+
+INSERT INTO `tbldepartments` (`id`, `DepartmentName`, `DepartmentShortName`, `DepartmentCode`, `CreationDate`) VALUES
+(1, 'Human Resource', 'HR', 'HR001', '2017-11-01 07:16:25'),
+(2, 'Information Technology', 'IT', 'IT001', '2017-11-01 07:19:37'),
+(3, 'Operations', 'OP', 'OP1', '2017-12-02 21:28:56');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblemployees`
+--
+
+CREATE TABLE `tblemployees` (
+  `id` int(11) NOT NULL,
+  `EmpId` varchar(100) NOT NULL,
+  `FirstName` varchar(150) NOT NULL,
+  `LastName` varchar(150) NOT NULL,
+  `EmailId` varchar(200) NOT NULL,
+  `Password` varchar(180) NOT NULL,
+  `Gender` varchar(100) NOT NULL,
+  `Dob` varchar(100) NOT NULL,
+  `Department` varchar(255) NOT NULL,
+  `Address` varchar(255) NOT NULL,
+  `City` varchar(200) NOT NULL,
+  `Country` varchar(150) NOT NULL,
+  `Phonenumber` char(11) NOT NULL,
+  `Status` int(1) NOT NULL,
+  `RegDate` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblemployees`
+--
+
+INSERT INTO `tblemployees` (`id`, `EmpId`, `FirstName`, `LastName`, `EmailId`, `Password`, `Gender`, `Dob`, `Department`, `Address`, `City`, `Country`, `Phonenumber`, `Status`, `RegDate`) VALUES
+(3, 'EMP12345', 'Collins', 'Joshua', 'collin@factentry.com', '49cb80f514aaebfee0e25e2e80837303', 'Male', '12 December, 1995', 'Information Technology', 'Sathuvachari', 'Vellore', 'India', '8668111563', 1, '2020-10-23 11:47:32');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblleaves`
+--
+
+CREATE TABLE `tblleaves` (
+  `ID` int(11) NOT NULL,
+  `LeaveType` varchar(110) NOT NULL,
+  `ToDate` varchar(120) NOT NULL,
+  `FromDate` varchar(120) NOT NULL,
+  `Description` mediumtext NOT NULL,
+  `PostingDate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `AdminRemark` mediumtext DEFAULT NULL,
+  `AdminRemarkDate` varchar(120) DEFAULT NULL,
+  `Status` int(1) NOT NULL,
+  `IsRead` int(1) NOT NULL,
+  `empid` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblleaves`
+--
+
+INSERT INTO `tblleaves` (`ID`, `LeaveType`, `ToDate`, `FromDate`, `Description`, `PostingDate`, `AdminRemark`, `AdminRemarkDate`, `Status`, `IsRead`, `empid`) VALUES
+(11, 'Casual Leave', '24/10/2020', '23/10/2020', 'Marriage ', '2020-10-23 11:49:50', 'approved', '2020-10-23 17:23:26 ', 1, 1, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblleavetype`
+--
+
+CREATE TABLE `tblleavetype` (
+  `id` int(11) NOT NULL,
+  `LeaveType` varchar(200) DEFAULT NULL,
+  `Description` mediumtext DEFAULT NULL,
+  `CreationDate` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblleavetype`
+--
+
+INSERT INTO `tblleavetype` (`id`, `LeaveType`, `Description`, `CreationDate`) VALUES
+(1, 'Casual Leave', 'Casual Leave ', '2020-10-23 12:03:17'),
+(2, 'Medical Leave test', 'Medical Leave  test', '2020-10-23 12:03:37'),
+(3, 'Restricted Holiday(RH)', 'Restricted Holiday(RH)', '2020-10-23 12:04:01');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbldepartments`
+--
+ALTER TABLE `tbldepartments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblemployees`
+--
+ALTER TABLE `tblemployees`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblleaves`
+--
+ALTER TABLE `tblleaves`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `UserEmail` (`empid`);
+
+--
+-- Indexes for table `tblleavetype`
+--
+ALTER TABLE `tblleavetype`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tbldepartments`
+--
+ALTER TABLE `tbldepartments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `tblemployees`
+--
+ALTER TABLE `tblemployees`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tblleaves`
+--
+ALTER TABLE `tblleaves`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `tblleavetype`
+--
+ALTER TABLE `tblleavetype`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
